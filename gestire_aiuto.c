7124de048c4f44e942bced656e545259977a4316
CrickCrock28 <lucardito03@gmail.com>
2023-06-03 23:18:30 +0200

    Inserimento file e aggiornamento modulo aiuto

--- gestire_aiuto.c
+++ gestire_aiuto.c
@@ -7,7 +7,7 @@ void gestire_menu_aiuto(){
         stampare_file_di_testo("menu_aiuto.txt");
         chiedere_intero("Inserire scelta (0 a 2):", 0, 2, 7, 0);
         if(scelta == 1){
-            stampare_file_di_testo("reegole_gioco.txt");
+            stampare_file_di_testo("regole_gioco.txt");
         }else if(scelta == 2){
             stampare_file_di_testo("manuale_gioco.txt");
         }
