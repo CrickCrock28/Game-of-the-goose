78bddd18c5d9106b8cd3e5a4327905cb8d56c701
CrickCrock28 <lucardito03@gmail.com>
2023-06-03 23:10:55 +0200

    Modulo gestire aiuto aggiornato

--- gestire_aiuto.c
+++ gestire_aiuto.c
@@ -1,1 +1,15 @@
-const prova;
+#include "gestire_aiuto.h"
+
+void gestire_menu_aiuto(){
+    int scelta;
+    do
+    {
+        stampare_file_di_testo("menu_aiuto.txt");
+        chiedere_intero("Inserire scelta (0 a 2):", 0, 2, 7, 0);
+        if(scelta == 1){
+            stampare_file_di_testo("reegole_gioco.txt");
+        }else if(scelta == 2){
+            stampare_file_di_testo("manuale_gioco.txt");
+        }
+    } while (scelta == 0);
+}
--- gestire_aiuto.h
+++ gestire_aiuto.h
@@ -1,1 +1,6 @@
-const int prova;
+#ifndef GESTIRE_AIUTO_H_
+#define GESTIRE_AIUTO_H_
+
+void gestire_menu_aiuto();
+
+#endif /* GESTIRE_AIUTO_H_ */
