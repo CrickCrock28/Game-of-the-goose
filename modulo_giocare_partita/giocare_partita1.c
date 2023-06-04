94f45b2a35b439d69e22f8bb0739f63b750faa10
CrickCrock28 <lucardito03@gmail.com>
2023-06-04 20:08:10 +0200

    ci cazz ohhh

--- modulo_giocare_partita/giocare_partita1.c
+++ modulo_giocare_partita/giocare_partita1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <funzioni_accesso_da_modificare.h>
 
+//TUTTE LE COSTANTI SONO FA RIVEDERE
+
 #define NUMERO_MINIMO_CASELLE 1
 #define NUMERO_MASSIMO_CASELLE 100
 #define CASELLA_OCA 'O'
@@ -12,21 +14,18 @@
 #define CASELLA_SCHELETRO 'S'
 #define CASELLA_PRIGIONE 'G'
 #define CASELLA_FINE 'F'
-#define ARRIVO_LABIRINTO 42
-#define NUMERO_MASSIMO_CASELLE 100
-#define TURNI_BLOCCATO_LOCANDA
-
+#define ARRIVO_LABIRINTO 999
+#define TURNI_BLOCCATO_LOCANDA 999
 #define NUMERO_MASSIMO_GIOCATORI 4
 #define PRIMO_INDICE_ARRAY 0
-#define FALSO 0
-#define VERO 1
 #define PRIMA_POSIZIONE_PERCORSO 1
 #define PERCORSO_FILE_MENU_PARTITA "menu_partita.txt"
-
 #define ARRIVO_PRIMO_LANCIO_4_5 2
 #define ARRIVO_PRIMO_LANCIO_3_6 3
 #define NUMERO_MINIMO_DADO 1
 #define NUMERO_MASSIMO_DADO 6
+#define FALSO 0
+#define VERO 1
 
 char tipo_casella(record_partita partita, int indice_giocatore) {
     record_vet_giocatori vet_giocatori;
@@ -127,8 +126,8 @@ record_partita giocare_partita(record_partita partita) {
            !leggere_salvare_partita_record_partita(partita)) {
         indice_giocatore_di_turno = leggere_indice_giocatore_di_turno_record_partita(partita);
         stampare_file_di_testo(PERCORSO_FILE_MENU_PARTITA);
-        scelta = chiedere_intero("Inserisci la scelta: ", 0, 3);    //DA CAMBIAREEEEEEEEEEEEEEEEE
-        partita = gestire_scelta_partita(partita, scelta);          //DA CAMBIAREEEEEEEEEEEEEEEEE
+        scelta = chiedere_intero("Inserisci la scelta: ", 0, 3);    //DA RIVEDERE
+        partita = gestire_scelta_partita(partita, scelta);          //DA RIVEDERE
     }
 
     return partita;
@@ -142,8 +141,8 @@ record_partita lanciare_primi_dadi(record_partita partita, int indice_giocatore)
     record_giocatore giocatore;
     int dimensione_percorso;
 
-    dado_1 = generare_numero_casuale(NUMERO_MINIMO_DADO, NUMERO_MASSIMO_DADO);      //DA RIVEDEREEEEEE
-    dado_2 = generare_numero_casuale(NUMERO_MINIMO_DADO, NUMERO_MASSIMO_DADO);      //DA RIVEDEREEEEEE
+    dado_1 = generare_numero_casuale(NUMERO_MINIMO_DADO, NUMERO_MASSIMO_DADO);      //DA RIVEDERE
+    dado_2 = generare_numero_casuale(NUMERO_MINIMO_DADO, NUMERO_MASSIMO_DADO);      //DA RIVEDERE
     
     partita = scrivere_ultimo_lancio_dado_1_record_partita(partita, dado_1);
     partita = scrivere_ultimo_lancio_dado_2_record_partita(partita, dado_2);
@@ -212,37 +211,33 @@ record_partita stabilire_primo_giocatore(record_partita partita) {
     return partita;
 }
 
-record_partita gestire_menu_nuova_partita(record_partita partita, char* menu_nuova_partita, char* menu_partita_in_corso, int riprendere_partita) {
+record_partita gestire_menu_nuova_partita(const char* menu_nuova_partita, const char* menu_partita_in_corso, int riprendere_partita) {
     int nuova_partita;
     int scelta;
-
+    record_partita partita;
+    
     nuova_partita = FALSO;
-    scelta = -1;
-
-    while (scelta != 0) {
-        if (verificare_partita_terminata(partita)) {
-            // La partita è terminata, aggiornare la classifica e tornare alla funzione principale
-            aggiornare_classifica(partita);
-            return partita;
-        } else if (leggere_salvare_partita_record_partita(partita)) {
-            // L'utente vuole salvare la partita, salvare e aggiornare le partite salvate, tornare alla funzione principale
-            salvare_partita(partita);
-            aggiornare_partite_salvate(partita);
-            return partita;
-        } else if (leggere_nuova_partita_record_partita(partita)) {
-            // L'utente vuole creare una nuova partita ex novo
-            nuova_partita = 1;
-        } else if (leggere_abbandona_partita_record_partita(partita) == 0) {
-            // L'utente vuole tornare al menu precedente/abbandonare la partita
-            stampare_file_di_testo(menu_nuova_partita);
-            scelta = chiedere_intero("Inserisci la scelta: ", 0, 1);
-            partita = gestire_scelta_nuova_partita(partita, scelta);
+    do {
+        if (nuova_partita) {
+            partita = gestire_scelta_nuova_partita(scelta); //DA RIVEDERE
         } else {
-            // L'utente vuole continuare la partita appena salvata
-            partita = giocare_partita(partita);
+            if (riprendere_partita == 0) {
+                stampare_file_di_testo(PERCORSO_FILE_MENU_PARTITA);
+                scelta = chiedere_intero("Inserisci la scelta: ", 0, 1); //DA RIVEDERE
+                partita = gestire_scelta_nuova_partita(scelta); //DA RIVEDERE
+            } else {
+                partita = giocare_partita(partita); //DA RIVEDERE
+            }
         }
-    }
+
+        if (leggere_nuova_partita_record_partita(partita)) {
+            nuova_partita = VERO;
+        } else {
+            if (leggere_abbandona_partita_record_partita(partita) == FALSO) {
+                scelta = 0;
+            }
+        }
+    } while (scelta != 0);
 
     return partita;
 }
-
