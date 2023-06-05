0c72e38850e65471cab8c1da20022962b3ebe3b8
CrickCrock28 <lucardito03@gmail.com>
2023-06-05 09:36:04 +0200

    effetti speciali

--- modulo_giocare_partita/giocare_partita2.c
+++ modulo_giocare_partita/giocare_partita2.c
@@ -12,7 +12,7 @@
 #define CASELLA_LABIRINTO '1'
 #define CASELLA_SCHELETRO '1'
 #define CASELLA_PRIGIONE '1'
-#define CASELLA_FINE '1'
+#define CASELLA_FINALE '90'
 #define ARRIVO_LABIRINTO 999
 #define TURNI_BLOCCATO_LOCANDA 999
 #define NUMERO_MASSIMO_GIOCATORI 4
@@ -116,7 +116,7 @@ record_partita lanciare_dadi(record_partita partita, int indice_giocatore){
         }
     }else{
         giocatore = scrivere_posizione_record_giocatore(giocatore, nuova_posizione);
-        vet_giocatori = scrivere_giocatore_record_vet_giocatori(giocatore);
+        vet_giocatori = scrivere_giocatore_record_vet_giocatori(vet_giocatori, indice_giocatore, giocatore);
         partita = scrivere_vet_giocatori_record_partita(partita, vet_giocatori);
     }
     return partita;
@@ -132,36 +132,39 @@ int calcolare_resto(int dividendo, int divisore){
 }
 
 
-gestire_effetti_caselle_speciali(record_partita partita, int indice_giocatore){
+record_partita gestire_effetti_caselle_speciali(record_partita partita, int indice_giocatore){
     if (trovare_tipo_casella_giocatore(partita, indice_giocatore)  == CASELLA_OCA || trovare_tipo_casella_giocatore(partita, indice_giocatore) = CASELLA_PONTE){
         partita = applicare_effetto_casella_oca_ponte(partita, indice_giocatore)
-    }else{
-    if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_LABIRINTO){
-    partita = applicare_effetto_casella_labirinto(partita, indice_giocatore);
-    }else{
-    if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_POZZO){
-    partita = applicare_effetto_casella_pozzo(partita, indice_giocatore);
-    }else{
-    if (trovare_tipo_casella_giocatore(partita, indice_giocatore) = CASELLA_PRIGIONE){
-    partita = applicare_effetto_casella_prigione(partita, indice_giocatore);
-    }else{  
-    if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_LOCANDA){
-    partita = applicare_effetto_casella_locanda(partita, indice_giocatore);
-    }else{ 
-     if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_SCHELETRO){
-    partita = applicare_effetto_casella_scheletro(partita, indice_giocatore);
-     }else{
-    if (trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_FINALE){
-    }else{
-    partita = applicare_effetto_casella_finale(partita);
-    }
-     }
-     }
-    }
-    }
-    }
-    }
+        }else{
+            if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_LABIRINTO){
+                partita = applicare_effetto_casella_labirinto(partita, indice_giocatore);
+                }else{
+                    if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_POZZO){
+                        partita = applicare_effetto_casella_pozzo(partita, indice_giocatore);
+                        }else{
+                            if (trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_PRIGIONE){
+                                
+                                partita = applicare_effetto_casella_prigione(partita, indice_giocatore);}else{  
+                                    if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_LOCANDA){partita = applicare_effetto_casella_locanda(partita, indice_giocatore);
+                                    }else{ 
+                                        if(trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_SCHELETRO){
+                                            partita = applicare_effetto_casella_scheletro(partita, indice_giocatore);
+                                            }else{
+                                                if (trovare_tipo_casella_giocatore(partita, indice_giocatore) == CASELLA_FINALE){
+                                                }else{
+                                                    partita = applicare_effetto_casella_finale(partita);
+                                                }
+                                            }
+                                    }
+                                }
+                        }
+                    }
+            }
+    return partita;
 }
+
+
+
 
 
 
