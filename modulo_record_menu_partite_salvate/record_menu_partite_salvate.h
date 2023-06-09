deccf2402a370d1075360b4386d5492b7e68c105
CrickCrock28 <lucardito03@gmail.com>
2023-06-09 17:32:34 +0200

    ciaoooo

--- modulo_record_menu_partite_salvate/record_menu_partite_salvate.h
+++ modulo_record_menu_partite_salvate/record_menu_partite_salvate.h
@@ -3,10 +3,14 @@
 
 #include "record_partita.h"
 
-typedef struct record_menu_partite_salvate{
+typedef struct record_partite_salvate{
     FILE* file_partite_salvate;
     record_partita partita_opzionale;
 } record_menu_partite_salvate;
 
+FILE* leggere_file_partite_salvate();
+void scrivere_file_partite_salvate();
+record_partita leggere_partita_opzionale();
+void scrivere_partita_opzionale();
 
 #endif /* RECORD_MENU_PARTITE_SALVATE */
