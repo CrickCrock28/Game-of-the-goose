c8374de6b49f25dd3a6508a9a48b38f15b46d2ca
CrickCrock28 <lucardito03@gmail.com>
2023-06-12 18:44:46 +0200

    Modifiche //

--- modulo_record_classificato/record_classificato.h
+++ modulo_record_classificato/record_classificato.h
@@ -2,11 +2,11 @@
 #define RECORD_CLASSIFICATO_H
 
 typedef struct record_classificato{
-	char nome_giocatore[LUNGHEZZA_NOME];
+	char nome_giocatore[LUNGHEZZA_NOME + 1];
 	int tiri;
 } record_classificato;
 
-void leggere_nome_giocatore_record_classificato(record_classificato classificato);
+void leggere_nome_giocatore_record_classificato(record_classificato classificato,char* nome);
 record_classificato scrivere_nome_giocatore_record_classificato(record_classificato classificato, char* nome);
 int leggere_tiri_record_classificato(record_classificato classificato);
 record_classificato scrivere_tiri_record_classificato(record_classificato classificato, int tiri);
