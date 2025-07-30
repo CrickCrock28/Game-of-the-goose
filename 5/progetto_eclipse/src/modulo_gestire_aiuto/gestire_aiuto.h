674965d264a58cd4c93707a956375828adb7fa4e
CrickCrock28 <lucardito03@gmail.com>
2025-07-30 12:19:48 +0200

    remove old files

--- 5/progetto_eclipse/src/modulo_gestire_aiuto/gestire_aiuto.h
+++
@@ -1,8 +0,0 @@
-#ifndef GESTIRE_AIUTO_H_
-#define GESTIRE_AIUTO_H_
-
-
-// Gestisce il menù di aiuto 
-void gestire_menu_aiuto(char* percorso_file_menu_aiuto, char* percorso_file_regole_gioco, char* percorso_file_manuale_gioco);
-
-#endif /* GESTIRE_AIUTO_H_ */
--- 5/progetto_eclipse/src/modulo_spostare_cursore/spostare_cursore.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPOSTARE_CURSORE_H_
-#define SPOSTARE_CURSORE_H_
-
-
-void spostare_cursore(int x, int y);
-
-
-#endif /* SPOSTARE_CURSORE_H_ */
